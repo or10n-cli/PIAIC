0f8f529ff427ec7d7a1103bd08c820b498e5e923
or10n-cli <Muhammad.ahmed.27@hotmail.com>
2025-05-05 12:45:41 +0500

    Add files via upload

---
+++ Q1/Class3/age_checker_183376.py
@@ -0,0 +1,14 @@
+# This Program Authenticates either you are Allowed or Not by verifying your Age
+
+# This age variable gets input from User
+age = int(input("How old are You?, Ans: "))
+
+# Conditional Age Verification & Authentication
+if age >= 18:
+    print("You are Allowed!")
+else:
+    print("Oops! You are not Allowed yet." \
+    " Come Next Time!")
+
+
+
