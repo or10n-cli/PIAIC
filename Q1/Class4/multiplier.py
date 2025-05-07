3a3ac98cf0f30f9bc975b1d07eff6e0b1f29ed84
or10n-cli <Muhammad.ahmed.27@hotmail.com>
2025-05-07 10:21:00 +0500

    multiplier
    Custom Function to Multiply Two Numbers

---
+++ Q1/Class4/multiplier.py
@@ -0,0 +1,10 @@
+# A custom function to multiply two numbers
+def multiplier(a, b):
+    output = (a * b)
+    print(output)
+    return output
+
+num1 = int(input("Enter 1st Number: "))
+num2 = int(input("Enter 2nd Number: "))
+
+multiplier(num1, num2)
